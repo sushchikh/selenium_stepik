e85f6f1ccd40c1ee42f2d95218d109a318478c19
sushchikh <a.sushchikh@gmail.com>
2019-10-18 17:03:28 +0300

    add 3_3_8, requirements.txt, etc

--- test.py
+++ test.py
@@ -1,14 +1,53 @@
 print(some_var := 8)
 print(some_var)
 
-def greet_f(name, greeting):
-    print(f'{greeting}, {name}')
-
-greet_f(name='Artem', greeting='hei-ho')
-
-def double(some):
-    return 2 * some
-
-print(double('2'))
+# debug using f'string:
+some_var = "Artem Sushchikh"
+print(f'{some_var =}')  # позволяет выводить переменную и ее значение через f'strings
 
 
+def add_one(number):
+    return number + 1
+print(add_one(2))
+
+
+print('='*80)
+print()
+
+def parent(num):
+    def first_child():
+        return "My name is Emma"
+
+    def second_child():
+        return "Call me Liam"
+
+    if num == 1:
+        return first_child
+    else:
+        return second_child
+
+first = parent(1)
+second = parent(2)
+print(f'{first = }')
+print(f'{second = }')
+
+print(first())
+
+
+print('='*80)
+print()
+
+
+def my_decorator(func):
+    def wrapper():
+        print('что-то происходит до функции')
+        func()
+        print('что-то происходит после функции')
+    return wrapper
+
+
+@my_decorator
+def say_whee():
+    print('Whee!')
+
+say_whee()
