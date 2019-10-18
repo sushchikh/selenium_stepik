1e874266c6a2fa7221dddc994ac7c051a8386a66
sushchikh <a.sushchikh@gmail.com>
2019-10-18 11:56:51 +0300

    try python 3.8

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-assert 0, "some message for developer"
+print(some_var := 8)
+print(some_var)
+
+def greet_f(name, greeting):
+    print(f'{greeting}, {name}')
+
+greet_f(name='Artem', greeting='hei-ho')
+
+def double(some):
+    return 2 * some
+
+print(double('2'))
+
 
